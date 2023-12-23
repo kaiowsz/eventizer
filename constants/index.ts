203bdeee6735055e30f9f8bb40f7b6f4cfe963fb
Kaio Silveira <107553497+kaiowsz@users.noreply.github.com>
2023-12-23 14:02:48 -0300

    feat: Add event create form

---
+++ constants/index.ts
@@ -0,0 +1,12 @@
+export const eventDefaultValues = {
+    title: "",
+    description: "",
+    location: "",
+    imageUrl: "",
+    categoryId: "",
+    price: "",
+    url: "",
+    isFree: false,
+    startDateTime: new Date(),
+    endDateTime: new Date(),
+}
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -62,4 +62,22 @@ export async function deleteUser(clerkId: string) {
     } catch (error) {
         handleError(error)
     }
-} 
+}
+
+export async function getUserById(userId: String) {
+    try {
+        
+        await connectToDatabase();
+
+        const user = await User.findById(userId);
+        
+        if(!user) {
+            throw new Error("User not found.")
+        }
+
+        return JSON.parse(JSON.stringify(user));
+
+    } catch (error) {
+        handleError(error)
+    }
+}
