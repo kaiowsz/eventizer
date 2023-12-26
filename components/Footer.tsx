0d6691558221be39e1e3e59dd9c267179ad6dbe1
Kaio Silveira <107553497+kaiowsz@users.noreply.github.com>
2023-12-26 09:11:26 -0300

    feat: Change layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,9 @@ const Footer = () => {
   return (
     <footer className="border-t">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
-        <Link href="/">
-          <Image src="/assets/images/logo.svg" alt="Logo" width={128} height={38} />
+        <Link href="/" className="w-36 flex justify-center items-center gap-1">
+          <Image src="/assets/icons/logo-grey.svg" width={38} height={38} alt="Logo image" />
+          <h1 className="font-bold text-lg">Eventizer</h1>
         </Link>
 
         <p>2023 Eventizer. All rights reserved. &reg;</p>
