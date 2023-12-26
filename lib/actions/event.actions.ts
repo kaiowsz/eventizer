649b3f815fdf9badba96eb2a570750efa820b278
Kaio Silveira <107553497+kaiowsz@users.noreply.github.com>
2023-12-26 09:12:35 -0300

    feat: Event actions

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,11 +1,12 @@
 "use server"
 
-import { CreateEventParams } from "@/@types";
+import { CreateEventParams, DeleteEventParams, GetAllEventsParams, GetEventsByUserParams, GetRelatedEventsByCategoryParams, UpdateEventParams } from "@/@types";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.model";
 import { handleError } from "../utils";
 import Category from "../database/models/category.model";
+import { revalidatePath } from "next/cache";
 
 async function populateEvent(query: any) {
     return query
@@ -51,5 +52,124 @@ export async function getEventById(eventId: string) {
 
     } catch (error) {
         handleError(error)
+    }
+}
+
+export async function getAllEvents({ query, limit = 6, page, category }: GetAllEventsParams) {
+    try {
+        await connectToDatabase();
+
+        const conditions = {}
+
+        const eventsQuery = Event.find(conditions)
+        .sort({ createdAt: "desc" })
+        .skip(0)
+        .limit(limit);
+
+        const events = await populateEvent(eventsQuery)
+
+        const eventsCount = await Event.countDocuments(conditions)
+
+
+        return {
+            data: JSON.parse(JSON.stringify(events)),
+            totalPages: Math.ceil(eventsCount / limit)
+        }
+
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+export async function deleteEvent({ path, eventId }: DeleteEventParams) {
+    try {
+        await connectToDatabase();
+
+        const deletedEvent = await Event.findByIdAndDelete(eventId);
+
+        if(deletedEvent) {
+            revalidatePath(path)
+        }
+
+
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+export async function updateEvent({ userId, event, path }: UpdateEventParams) {
+    try {
+        await connectToDatabase();
+
+        const eventToUpdate = await Event.findById(event._id);
+
+        if(!eventToUpdate || eventToUpdate.organizer.toHexString() !== userId) {
+            throw new Error("Unauthorized or event not found.")
+        }
+
+        const updatedEvent = await Event.findByIdAndUpdate(
+            event._id,
+            { ...event, category: event.categoryId },
+            { new: true }
+        )
+
+        revalidatePath(path);
+
+        return JSON.parse(JSON.stringify(updatedEvent));
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+export async function getEventsByUser({ userId, limit = 6, page }: GetEventsByUserParams) {
+    try {
+        await connectToDatabase();
+
+        const conditions = { organizer: userId }
+        const skipAmount = (page - 1) * limit
+        
+        const eventsQuery = Event.find(conditions)
+        .sort({createdAt: "desc"})
+        .skip(skipAmount)
+        .limit(limit)
+
+        const events = await populateEvent(eventsQuery);
+        const eventsCount = await Event.countDocuments(conditions);
+
+        return {
+            data: JSON.parse(JSON.stringify(events)),
+            totalPages: Math.ceil(eventsCount / limit)
+        }
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+export async function getRelatedEventsByCategory({categoryId, eventId, limit = 3, page = 1}: GetRelatedEventsByCategoryParams) {
+    try {
+        await connectToDatabase();
+
+        const skipAmount = (Number(page) - 1) * limit;
+        const conditions = { 
+            $and: [
+                { category: categoryId }, 
+                { _id: {$ne: eventId} }
+            ]
+        }
+
+        const eventsQuery = Event.find(conditions)
+        .sort({createdAt: "desc"})
+        .skip(skipAmount)
+        .limit(limit)
+
+        const events = await populateEvent(eventsQuery);
+        const eventsCount = await Event.countDocuments(conditions);
+
+        return {
+            data: JSON.parse(JSON.stringify(events)),
+            totalPages: Math.ceil(eventsCount / limit)
+        }
+    } catch (error) {
+        handleError(error);
     }
 }
